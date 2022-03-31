b5ce71abd3b6ef3cf39110e6783545d59c173b3f
Handryo Casali <handryo@hotmail.com>
2022-03-31 17:31:15 -0300

    Adicionando exercicio 02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Olá, em que turno voce trabalha? ");
+Console.WriteLine(@"
+Digite:
+'M' para Matutino.
+'V' para Vespertino.
+'N' para Norturno.
+");
+
+string turno = Console.ReadLine().ToUpper();
+
+switch(turno)
+{
+    case "M":
+        Console.WriteLine($"Bom dia!");
+        break;
+
+    case "V":
+        Console.WriteLine($"Boa tarde!");
+        break;
+
+    case "N":
+        Console.WriteLine($"Boa noite!");
+        break;
+
+    default:
+        Console.WriteLine($"Valor inválido");
+        break;
+}
